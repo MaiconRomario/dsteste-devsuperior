e084dec5cb8a7c45ca4f57a6aa86214b11bc1839
MaiconRomario <maiconromario389@hotmail.com>
2022-08-16 16:27:07 -0300

    Created backend and dependency

---
+++ backend/src/main/java/com/devmaicon/dsteste/DstesteApplication.java
@@ -0,0 +1,13 @@
+package com.devmaicon.dsteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DstesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DstesteApplication.class, args);
+	}
+
+}
